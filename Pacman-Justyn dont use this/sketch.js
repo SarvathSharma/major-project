e0d818df0ac2850abeba8d5c562e6033eb195d85
Sharma5604247 <5604247@sps-student.sk.ca>
2018-06-08 15:21:04 -0600

    edits

--- Pacman-Justyn dont use this/sketch.js
+++ Pacman-Justyn dont use this/sketch.js
@@ -5,8 +5,8 @@
 
 // Arrays //
 
-// 2 = blackspace, 1 = border, 2 = point, 3 = pacman, 4 = ghost
-let startingGrid = [
+// 0 = blackspace, 1 = border, 2 = point, 3 = pacman, 4 = greenGhost
+let grid = [
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, ],
   [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, ],
   [1, 2, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, ],
@@ -14,7 +14,7 @@ let startingGrid = [
   [1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, ],
   [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, ],
   [1, 2, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, ],
-  [1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, ],
+  [1, 2, 2, 2, 2, 2, 2, 2, 1, 4, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, ],
   [1, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 0, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, ],
   [1, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 0, 0, 0, 0, 0, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, ],
   [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 0, 0, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, ],
@@ -32,8 +32,14 @@ let startingGrid = [
 
 // global variables //
 let backgroundImage, pacman, pointImage, score;
-let pacmanUp, pacmanLeft, pacmanRight, pacmanDown, currentPacman;
+let openMouth, closedMouth, pacmanEating;
 let cellSize = 25;
+let downFree, rightFree, upFree, leftFree;
+let directionList = [];
+let direction;
+let previousDirection = "none";
+let pacmanUp, pacmanDown, pacmanRight, pacmanLeft, currentPacman;
+let greenGhost, inky;
 
 function preload() {
   backgroundImage = loadImage("images/pacman-grid.png");
@@ -42,31 +48,67 @@ function preload() {
   pacmanRight = loadImage("images/pacman-right.png");
   pacmanLeft = loadImage("images/pacman-left.png");
   pointImage = loadImage("images/point.png");
+  greenGhost = loadImage("images/greenGhost.png");
+  // pacmanEating = createImg("images/pacman-eating.gif");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   pacman = new Pacman();
   score = new Score();
+  inky = new Bashful();
   currentPacman = pacmanRight;
 }
 
+Array.prototype.sample = function() {
+  return this[Math.floor(Math.random() * this.length)];
+};
+
 function draw() {
+  // pacman.showPacman();
   pacman.movePac();
   makeGrid();
   score.showOnScreen();
+  inky.moveGhost();
 }
+
+// function makeGrid() {
+//   noStroke();
+//   image(backgroundImage, 0, 0);
+//   for (let x = 0; x < 27; x++) {
+//     for (let y = 0; y < 21; y++) {
+//       if (grid[y][x] === 2) {
+//         image(pointImage, cellSize * x, cellSize * y);
+//       }
+//       if (grid[y][x] === 3) {
+//         // if (frameCount % 10 === 0) {
+//         //   image(openMouth, cellSize * x, cellSize * y, 25, 25);
+//         // }
+//         // if (frameCount % 20 === 0) {
+//         //   image(closedMouth, cellSize * x, cellSize * y, 25, 25);
+//         // }
+//         pacmanEating.position(cellSize * x, cellSize * y);
+//       }
+//       if (grid[y][x] === 4) {
+//         image(greenGhost, cellSize * x, cellSize * y);
+//       }
+//     }
+//   }
+// }
 
 function makeGrid() {
   noStroke();
   image(backgroundImage, 0, 0);
   for (let x = 0; x < 27; x++) {
     for (let y = 0; y < 21; y++) {
-      if (startingGrid[y][x] === 2) {
+      if (grid[y][x] === 2) {
         image(pointImage, cellSize * x, cellSize * y);
       }
-      if (startingGrid[y][x] === 3) {
+      if (grid[y][x] === 3) {
         image(currentPacman, cellSize * x, cellSize * y, 25, 25);
+      }
+      if (grid[y][x] === 4) {
+        image(greenGhost, cellSize * x, cellSize * y);
       }
     }
   }
@@ -105,48 +147,44 @@ class Pacman {
     xYLoop: for (let x = 0; x < 27; x++) {
       for (let y = 0; y < 21; y++) {
         if (frameCount % 20 === 0) {
-          if (startingGrid[y][x] === 3) {
+          if (grid[y][x] === 3) {
             if (this.xSpeed === 10) {
-              if (startingGrid[y][x + 1] === 1) {
+              if (grid[y][x + 1] === 1) {
                 this.xSpeed = 0;
               }
               else {
-                startingGrid[y][x] = 0;
-                score.amount += 5;
-                startingGrid[y][x + 1] = 3;
+                grid[y][x] = 0;
+                grid[y][x + 1] = 3;
               }
               break xYLoop;
             }
             if (this.xSpeed === -10) {
-              if (startingGrid[y][x - 1] === 1) {
+              if (grid[y][x - 1] === 1) {
                 this.xSpeed = 0;
               }
               else {
-                startingGrid[y][x] = 0;
-                score.amount += 5;
-                startingGrid[y][x - 1] = 3;
+                grid[y][x] = 0;
+                grid[y][x - 1] = 3;
                 break xYLoop;
               }
             }
             if (this.ySpeed === 10) {
-              if (startingGrid[y + 1][x] === 1) {
+              if (grid[y + 1][x] === 1) {
                 this.ySpeed = 0;
               }
               else {
-                startingGrid[y][x] = 0;
-                score.amount += 5;
-                startingGrid[y + 1][x] = 3;
+                grid[y][x] = 0;
+                grid[y + 1][x] = 3;
                 break xYLoop;
               }
             }
             if (this.ySpeed === -10) {
-              if (startingGrid[y - 1][x] === 1) {
+              if (grid[y - 1][x] === 1) {
                 this.ySpeed = 0;
               }
               else {
-                startingGrid[y][x] = 0;
-                score.amount += 5;
-                startingGrid[y - 1][x] = 3;
+                grid[y][x] = 0;
+                grid[y - 1][x] = 3;
                 break xYLoop;
               }
             }
@@ -157,16 +195,70 @@ class Pacman {
   }
 }
 
+class Bashful {
+  constructor() {
+
+  }
+
+  moveGhost() {
+    let directionList = [];
+    ghostLoop: for (let x = 0; x < 27; x++) {
+      for (let y = 0; y < 21; y++) {
+        if (frameCount % 20 === 0) {
+          if (grid[y][x] === 4) {
+            if ((grid[y][x + 1] === 0 || grid[y][x + 1] === 2) && grid[y][x + 1] !== 1) {
+              directionList.push("rightFree");
+            }
+            if ((grid[y][x - 1] === 0 || grid[y][x - 1] === 2) && grid[y][x - 1] !== 1) {
+              directionList.push("leftFree");
+            }
+            if ((grid[y + 1][x] === 0 || grid[y + 1][x] === 2) && grid[y + 1][x] !== 1) {
+              directionList.push("downFree");
+            }
+            if ((grid[y - 1][x] === 0 || grid[y - 1][x] === 2) && grid[y - 1][x] !== 1) {
+              directionList.push("upFree");
+            }
+            if (directionList.indexOf(previousDirection) === -1) {
+              direction = directionList.sample();
+            }
+            else {
+              direction = previousDirection;
+            }
+            if (direction === "upFree") {
+              grid[y][x] = grid[y - 1][x];
+              grid[y - 1][x] = 4;
+            }
+            else if (direction === "downFree") {
+              grid[y][x] = grid[y + 1][x];
+              grid[y + 1][x] = 4;
+            }
+            else if (direction === "rightFree") {
+              grid[y][x] = grid[y][x + 1];
+              grid[y][x + 1] = 4;
+            }
+            else if (direction === "leftFree") {
+              grid[y][x] = grid[y][x - 1];
+              grid[y][x - 1] = 4;
+            }
+            previousDirection = direction;
+            break ghostLoop;
+          }
+          if (grid[y][x] === 3) {
+            image(currentPacman, cellSize * x, cellSize * y, 25, 25);
+          }
+        }
+      }
+    }
+  }
+}
+
 class Score {
   constructor() {
     this.amount = 0;
-    this.currentScore = 0;
-    this.highScore = 0;
   }
 
   showOnScreen() {
     textAlign(LEFT,BOTTOM);
-    textSize(80);
-    text("Score: " + this.amount, width-800, height-5);
+    text("Score: " + this.amount, width-175, height-5);
   }
 }
